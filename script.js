9d2bc3d17bca33853bd9179f8bebcf3af3954b63
udhaykanthv2008-cell <udhaykanthv2008@gmail.com>
2026-07-19 11:05:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const heart = document.querySelector(".heart");
+const music = document.getElementById("bgMusic");
+const message = document.getElementById("message");
+
+const text = `
+You are the best thing that ever happened to me. ❤️
+
+Every moment with you is my favorite memory.
+
+I promise to love you, support you and make you smile every day. 🥹💖
+
+Happy Birthday, My Love! 🎂🎈
+`;
+
+heart.addEventListener("click", function () {
+    music.volume = 0.4;
+    music.play();
+
+    heart.style.display = "none";
+    document.querySelector("h2").style.display = "none";
+    message.style.display = "block";
+
+    const p = message.querySelector("p");
+    p.innerHTML = "";
+
+    let i = 0;
+    const typing = setInterval(() => {
+        if (i < text.length) {
+            if (text[i] === "\n") {
+                p.innerHTML += "<br>";
+            } else {
+                p.innerHTML += text[i];
+            }
+            i++;
+        } else {
+            clearInterval(typing);
+        }
+    }, 40);
+
+    createHearts();
+});
+
+function createHearts() {
+    setInterval(() => {
+        const h = document.createElement("div");
+        h.classList.add("floating-heart");
+        h.innerHTML = "❤️";
+        h.style.left = Math.random() * 100 + "vw";
+        document.body.appendChild(h);
+
+        setTimeout(() => {
+            h.remove();
+        }, 6000);
+    }, 300);
+}
+// Fireworks coming next
+console.log("Fireworks Ready 🎆");
